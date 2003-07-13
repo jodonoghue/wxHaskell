047a3d273ea2ffe8a8f288bacd0dd541919cade1
dleijen <none>
2003-07-13 14:21:08 -0700

    [wxhaskell-from-cvs @ 2003-07-13 21:21:08 by dleijen]
    Initial revision
    
    darcs-hash:20030713212108-deb31-bde8afa9c371351662695a141ad9d00b0750c699.gz

---
+++ wxc/src/ewxw/eljimagelist.cpp
@@ -0,0 +1,70 @@
+#include "wrapper.h"
+
+extern "C"
+{
+
+EWXWEXPORT(void*, wxImageList_Create)(int width, int height, int mask, int initialCount)
+{
+	return (void*) new wxImageList(width, height, mask != 0, initialCount);
+}
+	
+EWXWEXPORT(void, wxImageList_Delete)(void* _obj)
+{
+	delete (wxImageList*)_obj;
+}
+
+EWXWEXPORT(int, wxImageList_GetImageCount)(void* _obj)
+{
+	return ((wxImageList*)_obj)->GetImageCount();
+}
+	
+EWXWEXPORT(int, wxImageList_GetSize)(void* _obj, int index, int* width, int* height)
+{
+	return (int)((wxImageList*)_obj)->GetSize(index, *((int*)width), *((int*)height));
+}
+	
+EWXWEXPORT(int, wxImageList_AddBitmap)(void* _obj, void* bitmap, void* mask)
+{
+	return ((wxImageList*)_obj)->Add(*((wxBitmap*)bitmap), *((wxBitmap*)mask));
+}
+	
+EWXWEXPORT(int, wxImageList_AddMasked)(void* _obj, void* bitmap, void* maskColour)
+{
+	return ((wxImageList*)_obj)->Add(*((wxBitmap*)bitmap), *((wxColour*)maskColour));
+}
+	
+EWXWEXPORT(int, wxImageList_AddIcon)(void* _obj, void* icon)
+{
+	return ((wxImageList*)_obj)->Add(*((wxIcon*)icon));
+}
+	
+EWXWEXPORT(int, wxImageList_Replace)(void* _obj, int index, void* bitmap, void* mask)
+{
+#ifdef __WIN32__
+	return (int)((wxImageList*)_obj)->Replace(index, *((wxBitmap*)bitmap), *((wxBitmap*)mask));
+#else
+	return (int)((wxImageList*)_obj)->Replace(index, *((wxBitmap*)bitmap));
+#endif
+}
+	
+EWXWEXPORT(int, wxImageList_ReplaceIcon)(void* _obj, int index, void* icon)
+{
+	return (int)((wxImageList*)_obj)->Replace(index, *((wxIcon*)icon));
+}
+	
+EWXWEXPORT(int, wxImageList_Remove)(void* _obj, int index)
+{
+	return (int)((wxImageList*)_obj)->Remove(index);
+}
+	
+EWXWEXPORT(int, wxImageList_RemoveAll)(void* _obj)
+{
+	return (int)((wxImageList*)_obj)->RemoveAll();
+}
+	
+EWXWEXPORT(int, wxImageList_Draw)(void* _obj, int index, void* dc, int x, int y, int flags, int solidBackground)
+{
+	return (int)((wxImageList*)_obj)->Draw(index, *((wxDC*)dc), x, y, flags, solidBackground != 0);
+}
+	
+}
