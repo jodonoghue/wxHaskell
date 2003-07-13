b287d7bd18d6c43863c7ab2783d7337e72a01918
dleijen <none>
2003-07-13 14:21:06 -0700

    [wxhaskell-from-cvs @ 2003-07-13 21:21:06 by dleijen]
    Initial revision
    
    darcs-hash:20030713212106-deb31-de87bac9278b499337537b643470d292a9fbf7dd.gz

---
+++ wxc/src/ewxw/eljicon.cpp
@@ -0,0 +1,77 @@
+#include "wrapper.h"
+
+extern "C"
+{
+
+EWXWEXPORT(void*, wxIcon_CreateDefault)()
+{
+	return (void*) new wxIcon();
+}
+
+EWXWEXPORT(void, wxIcon_Delete)(void* _obj)
+{
+	delete (wxIcon*)_obj;
+}
+
+EWXWEXPORT(void*, wxIcon_FromRaw)(void* data, int width, int height)
+{
+	return (void*) new wxIcon((const char*) data, width, height);
+}
+
+EWXWEXPORT(void*, wxIcon_FromXPM)(void* data)
+{
+	return (void*) new wxIcon((char**) data);
+}
+
+EWXWEXPORT(void*, wxIcon_CreateLoad) (void* name, long type, int width, int height)
+{
+	return (void*) new wxIcon((char*)name, type, width, height);
+}
+
+EWXWEXPORT(int, wxIcon_Load)(void* _obj, void* name, long type, int width, int height)
+{
+#ifdef __WIN32__
+	return (int)((wxIcon*)_obj)->LoadFile((char*)name, type, width, height);
+#else
+	return (int)((wxIcon*)_obj)->LoadFile((char*)name, type);
+#endif
+}
+
+EWXWEXPORT(void, wxIcon_CopyFromBitmap)(void* _obj, void* bmp)
+{
+#ifdef __WIN32__
+	((wxIcon*)_obj)->CopyFromBitmap(*((wxBitmap*)bmp));
+#endif
+}
+
+EWXWEXPORT(int, wxIcon_Ok)(void* _obj)
+{
+	return (int)((wxIcon*)_obj)->Ok();
+}
+
+EWXWEXPORT(int, wxIcon_GetDepth)(void* _obj)
+{
+	return (int)((wxIcon*)_obj)->GetDepth();
+}
+
+EWXWEXPORT(int, wxIcon_GetWidth)(void* _obj)
+{
+	return (int)((wxIcon*)_obj)->GetWidth();
+}
+
+EWXWEXPORT(int, wxIcon_GetHeight)(void* _obj)
+{
+	return (int)((wxIcon*)_obj)->GetHeight();
+}
+
+EWXWEXPORT(void, wxIcon_Assign)(void* _obj, void* other)
+{
+	*((wxIcon*)_obj) = *((wxIcon*)other);
+}
+
+EWXWEXPORT(int, wxIcon_IsEqual)(void* _obj, void* other)
+{
+	return (int)(*((wxIcon*)_obj) == *((wxIcon*)other));
+}
+
+}
