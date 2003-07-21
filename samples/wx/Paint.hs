f68541d9e522c2423b42c0db358762857a9c55b6
dleijen <none>
2003-07-20 17:06:38 -0700

    [wxhaskell-from-cvs @ 2003-07-21 00:06:37 by dleijen]
    New license and sample.
    
    darcs-hash:20030721000638-deb31-2c3cbffecd9b34db2eed1fb622c5163146a3126b.gz

---
+++ samples/wx/Paint.hs
@@ -0,0 +1,30 @@
+module Main where
+
+import Graphics.UI.WXH
+import Graphics.UI.WX
+
+main :: IO ()
+main
+  = start gui
+
+gui :: IO ()
+gui
+  = do f  <- frame [text := "Paint demo"]
+       sw <- scrolledWindow f [on paint := onpaint, virtualSize := sz 500 500, scrollRate := sz 10 10]
+       set f [clientSize := sz 150 150, layout := widget sw]
+       return ()
+  where
+    onpaint dc viewRect updateAreas
+      = do circle dc (pt 200 200) 20 [penKind := PenDash DashDot]
+           arc dc (pt 100 100) 20 90 230 [color := red, penWidth :~ (+1), penKind := PenSolid]
+           ellipticArc dc (rect  (pt 20  20) (sz 60 30)) 90 230 [color := blue, penWidth :~ (*2)]
+           c <- get dc color
+           -- set dc [font := fontDefault{ fontFace = "Courier New", fontSize = 16, fontWeight = WeightBold }]
+           set dc [fontFace := "Courier New", fontSize := 16, fontWeight := WeightBold ]
+           drawText dc (show c) (pt 50 50) []
+           rotatedText dc "rotated text" (pt 80 160) 45 [textColor := green]
+
+
+           
+
+           
