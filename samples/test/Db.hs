d7e26ea9ad8874e512325f9c6ed1d1c65f188155
dleijen <none>
2003-10-25 01:16:16 -0700

    [wxhaskell-from-cvs @ 2003-10-25 08:16:16 by dleijen]
    Added test files
    
    darcs-hash:20031025081616-deb31-f0e179c39b0b006e74f6df44f67a477acaa54ca9.gz

---
+++ samples/test/Db.hs
@@ -0,0 +1,99 @@
+module Main where
+
+import Graphics.UI.WX
+import Graphics.UI.WXCore
+import Graphics.UI.WXCore.WxcTypes
+
+import Foreign
+import Foreign.Ptr
+import Foreign.C.String
+import Foreign.Marshal.Array
+
+main 
+  = start $ 
+    do f   <- frame [text := "Pubs database"]
+
+       -- use text control as logger
+       log <- textCtrl f WrapLine [enable := False] 
+       textCtrlMakeLogActiveTarget log
+       
+       -- layout
+       set f [layout     := fill (widget log)
+             ,clientSize := sz 300 200
+             ]
+       
+       -- test
+       dir <- getApplicationDir
+       logMessage ("application dir: " ++ dir)
+
+       srcs <- dbGetDataSources
+       mapM_ (\(name,desc) -> logMessage ("source: " ++ name ++ ": " ++ desc)) srcs
+
+console :: IO ()
+console
+  = do srcs <- dbGetDataSources
+       putStrLn $ unlines $ ["source: " ++ name ++ ": " ++ desc | (name,desc) <- srcs]
+
+       putStrLn "opening pubs datasource"
+       connectInf <- dbConnectInfCreate nullHENV "pubs" "" "" "" "" "" 
+       db         <- dbGetConnection connectInf True
+       if (objectIsNull db)
+         then putStrLn "unable to open database!"
+         else do ok <- dbExecSql db "SELECT au_fname FROM authors"
+                 if not ok
+                   then putStrLn "unable to exec query"
+                   else do names <- dbGetNames db
+                           putStrLn $ unlines $ names                          
+                           dbFreeConnection db
+                           dbConnectInfDelete connectInf
+                           dbCloseConnections 
+                           putStrLn "done"
+
+dbGetNames :: Db a -> IO [String]
+dbGetNames db
+  = do ok <- dbGetNext db
+       if not ok
+        then return []
+        else do name  <- dbGetDataStringEx db 1024 1
+                names <- dbGetNames db
+                return (name:names)
+
+dbGetDataStringEx :: Db a -> Int -> Int -> IO String
+dbGetDataStringEx db maxLen column
+  = allocaArray maxLen $ \cstr ->
+    do ok <- dbGetDataString db column cstr maxLen nullPtr
+       if not ok
+        then ioError (userError "database: unable to retrieve string value.")
+        else peekCString cstr 
+
+-- | Returns the name and description of the data sources on the system.
+dbGetDataSources :: IO [(String,String)]
+dbGetDataSources 
+  = do connectInf <- dbConnectInfCreate nullHENV "" "" "" "" "" ""
+       henv       <- dbConnectInfGetHenv connectInf
+       xs         <- loop henv True
+       dbConnectInfDelete connectInf
+       return xs
+  where
+    loop henv isFirst
+      = do mbSrc <- dbGetDataSourceEx henv isFirst
+           case mbSrc of
+             Nothing  -> return [("test","test")]
+             Just x   -> do xs <- loop henv False
+                            return (x:xs)
+
+dbGetDataSourceEx :: HENV () -> Bool -> IO (Maybe (String,String))
+dbGetDataSourceEx henv isFirst
+  = allocaArray (dsnLen+1)  $ \cdsn  ->
+    allocaArray (descLen+1) $ \cdesc ->
+    do pokeArray0 0 cdsn []
+       pokeArray0 0 cdesc []
+       ok   <- dbGetDataSource henv (castPtr cdsn) dsnLen (castPtr cdesc) descLen isFirst
+       if not ok
+        then return Nothing
+        else do dsn  <- peekCString cdsn
+                desc <- peekCString cdesc 
+                return (Just (dsn,desc))
+  where
+    dsnLen  = 255
+    descLen = 1024
---
+++ samples/test/Resize.hs
@@ -0,0 +1,36 @@
+module Main where
+
+import Graphics.UI.WX
+import Graphics.UI.WXCore
+
+main 
+  = start $ 
+    do p <- getApplicationDir
+       f <- frame [text := p]
+       h <- staticText f [text := "Hello world!", on click ::= goodbye]
+       b <- button     f [text := "Ok", on command ::= longlabel]
+       set f [layout := column 5 [widget h, widget b]] 
+
+longlabel b
+  = do set b [text        := "a really long label for a button"
+             ,on command ::= shortlabel]
+       refit b
+
+shortlabel b
+  = do set b [text        := "short"
+             ,on command ::= longlabel]
+       refit b
+
+        
+
+goodbye st pos
+  = do set st [text      := "Goodbye world!\nAlas, I knew it well..."
+              ,on click ::= hello]
+       refit st
+
+
+hello st pos
+  = do set st [text      := "Hi!"
+              ,on click ::= goodbye]
+       refitMinimal st
+
