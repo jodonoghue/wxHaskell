63eaf293ab7f7213ac11ccdd840bb3c73471c8d8
dleijen <none>
2005-01-21 07:49:21 -0800

    [wxhaskell-from-cvs @ 2005-01-21 15:49:21 by dleijen]
    Text stream support
    
    darcs-hash:20050121154921-deb31-59ca16fdc83b0300c0728a0a107747c548b34d4d.gz

---
+++ wxc/src/textstream.cpp
@@ -0,0 +1,50 @@
+#include "wrapper.h"
+#include <wx/txtstrm.h>
+
+extern "C"
+{
+EWXWEXPORT( bool, wxInputStream_CanRead)( wxInputStream* self )
+{
+  if (self)
+    return self->CanRead();
+  else
+    return false;
+}
+
+
+
+EWXWEXPORT( wxTextInputStream*, wxTextInputStream_Create)( wxInputStream* inputStream, wxString* sep )
+{
+  return new wxTextInputStream( *inputStream, *sep );
+}
+
+EWXWEXPORT( void, wxTextInputStream_Delete)( wxTextInputStream* self )
+{
+  if (self) delete self;
+}
+
+EWXWEXPORT( wxString*, wxTextInputStream_ReadLine)( wxTextInputStream* self )
+{
+  if (!self) 
+    return new wxString("");
+  else
+    return new wxString( self->ReadLine() );
+}
+
+EWXWEXPORT( wxTextOutputStream*, wxTextOutputStream_Create)( wxOutputStream* outputStream, wxEOL mode )
+{
+  return new wxTextOutputStream( *outputStream, mode );
+}
+
+EWXWEXPORT( void, wxTextOutputStream_Delete)( wxTextOutputStream* self )
+{
+  if (self) delete self;
+}
+
+EWXWEXPORT( void, wxTextOutputStream_WriteString)( wxTextOutputStream* self, wxString* txt )
+{
+  if (!self) return;
+  self->WriteString( *txt );
+}
+
+}
