fd6e94f7d32e24497fe52503d73b9ac8ff8edd25
Mads Lindstroem <mads_lindstroem@yahoo.dk>
2007-09-16 10:08:28 -0700

    Changed Cursor_CreateLoad to return NULL for WXGTK, as WXGTK do yet implement this function
    darcs-hash:20070916170828-73052-1457309310dd0c60e221a685d6d50c6622681212.gz

--- wxc/src/ewxw/eljcursor.cpp
+++ wxc/src/ewxw/eljcursor.cpp
@@ -15,7 +15,12 @@ EWXWEXPORT(void*, Cursor_CreateFromImage)(wxImage* image)
 
 EWXWEXPORT(void*, Cursor_CreateLoad) (const wxString* name, long type, int width, int height)
 {
+#ifdef __WXGTK__
+// See http://thread.gmane.org/gmane.comp.lib.wxwidgets.general/45999
+	return NULL;
+#else
 	return (void*) new wxCursor(*name, type, width, height);
+#endif
 }
 
 }
