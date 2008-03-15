c3ea8201205403aa98d88ccde681095a285e57de
Eric Kow <eric.kow@gmail.com>
2008-03-15 16:02:25 -0700

    Make BoxedCombinator test fancier and move it out of bugs.
    darcs-hash:20080315230225-db91e-23b579a008ecb8743a42b945f9bd57b3eb5eacac.gz

--- samples/test/BoxedCombinator.hs
+++ samples/test/BoxedCombinator.hs
@@ -1,10 +1,10 @@
 module Main where
 import Graphics.UI.WX
 
-bugtext = unlines [ "Bug: these button should react when clicked"
+bugtext = unlines [ "Former bug: these button should react when clicked"
                   , "but the boxed one does not"
                   , ""
-                  , "Buggy   in: MacOS X"
+                  , "Buggy   in: MacOS X [now fixed!]"
                   , "Working in: Linux"
                   ]
 
@@ -12,9 +12,12 @@ main = start $ do
          f <- frame [ text := "program" ]
          b1 <- button f [ text := "click me" ]
          b2 <- button f [ text := "click me" ]
+         b3 <- button f [ text := "click me" ]
          set b1 [ on command := set b1 [ text := "thanks!" ] ]
          set b2 [ on command := set b2 [ text := "thanks!" ] ]
+         set b3 [ on command := set b3 [ text := "thanks!" ] ]
          set f [ visible := True
                , layout := margin 10 $ column 4
-                  [ label bugtext, widget b1, (boxed "" $ widget b2) ]
+                  [ label bugtext, widget b1, (boxed "" $ column 2 [ (boxed "hey" $ widget b2)
+                                                                   , widget b3 ]) ]
                ]
